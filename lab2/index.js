c20058c1fe9ecda5c1d9a19ac6a7ed7dbe751f22
Oleh Kniaziev <oleh.kniaziev@microsoft.wsei.edu.pl>
2024-03-07 11:30:35 +0100

    Added previous and next buttons (lab2)

--- lab2/index.js
+++ lab2/index.js
@@ -1,9 +1,22 @@
 const slider = document.querySelector('#slider-inner');
 const images = document.querySelectorAll("img");
 const imageButtons = document.querySelector("#image-buttons").getElementsByTagName("input");
-
+const prevButton = document.querySelector("#previous-btn");
+const nextButton = document.querySelector("#next-btn");
 
 let idx = 0;
+slider.appendChild(images[idx]);
+
+prevButton.addEventListener("click", () => {
+    slider.innerHTML = '';
+    idx = idx - 1 >= 0 ? idx - 1 : images.length - 1;
+    slider.appendChild(images[idx % images.length]);
+})
+
+nextButton.addEventListener("click", () => {
+    slider.innerHTML = '';
+    slider.appendChild(images[++idx % images.length]);
+})
 
 for (const btn of imageButtons) {
     btn.addEventListener("change", () => {

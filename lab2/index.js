dab81baeed6f252d02171806c406d8da51e4bdd4
Oleh Kniaziev <oleh.kniaziev@microsoft.wsei.edu.pl>
2024-03-07 11:25:51 +0100

    Added buttons to select an active image (lab2)

--- lab2/index.js
+++ lab2/index.js
@@ -1,7 +1,17 @@
 const slider = document.querySelector('#slider-inner');
 const images = document.querySelectorAll("img");
+const imageButtons = document.querySelector("#image-buttons").getElementsByTagName("input");
+
 
 let idx = 0;
+
+for (const btn of imageButtons) {
+    btn.addEventListener("change", () => {
+        slider.innerHTML = '';
+        idx = btn.value - 1;
+        slider.appendChild(images[idx]);
+    })
+}
 
 slider.appendChild(images[0]);
 

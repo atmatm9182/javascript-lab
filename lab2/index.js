73e40434e385c06bc6db754fba47ff20179c5a1a
atmatm9182 <atmatm9182@gmail.com>
2024-03-13 22:57:03 +0100

    [LAB2] The image buttons automagically are updated now

--- lab2/index.js
+++ lab2/index.js
@@ -5,40 +5,48 @@ const prevButton = document.querySelector("#previous-btn");
 const nextButton = document.querySelector("#next-btn");
 const pauseButton = document.querySelector("#pause-btn")
 
-let idx = 0;
-slider.appendChild(images[idx]);
+let slideIdx = 0;
+
+setCurrentSlide(slideIdx);
+
+/**
+ * @param {number} idx
+ */
+function setCurrentSlide(idx) {
+    slider.appendChild(images[idx]);
+    imageButtons[idx].checked = true;
+}
 
 prevButton.addEventListener("click", () => {
     slider.innerHTML = '';
-    idx = idx - 1 >= 0 ? idx - 1 : images.length - 1;
-    slider.appendChild(images[idx % images.length]);
+    slideIdx = slideIdx - 1 >= 0 ? slideIdx - 1 : images.length - 1;
+    setCurrentSlide(slideIdx % images.length);
 })
 
 nextButton.addEventListener("click", () => {
     slider.innerHTML = '';
-    slider.appendChild(images[++idx % images.length]);
+    setCurrentSlide(++slideIdx % images.length);
 })
 
 for (const btn of imageButtons) {
     btn.addEventListener("change", () => {
         slider.innerHTML = '';
-        idx = btn.value - 1;
-        slider.appendChild(images[idx]);
+        slideIdx = btn.value - 1;
+        setCurrentSlide(slideIdx);
     })
 }
 
 let isPaused = false;
 
-pauseButton.addEventListener("click", () => { 
+pauseButton.addEventListener("click", () => {
     isPaused = !isPaused;
-    console.log("voop")
 });
 
 const anim = setInterval(() => {
     if (isPaused)
         return;
 
-    slider.removeChild(images[idx % images.length]);
-    slider.appendChild(images[(idx + 1) % images.length]);
-    idx++;
+    slider.removeChild(images[slideIdx % images.length]);
+    setCurrentSlide((slideIdx + 1) % images.length);
+    slideIdx++;
 }, 3000);

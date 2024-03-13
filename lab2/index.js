2bf8b870244f3c5e3a1511e3d4890d9c68183336
atmatm9182 <atmatm9182@gmail.com>
2024-03-13 22:36:41 +0100

    [LAB2] Added a "Pause" button

--- lab2/index.js
+++ lab2/index.js
@@ -3,6 +3,7 @@ const images = document.querySelectorAll("img");
 const imageButtons = document.querySelector("#image-buttons").getElementsByTagName("input");
 const prevButton = document.querySelector("#previous-btn");
 const nextButton = document.querySelector("#next-btn");
+const pauseButton = document.querySelector("#pause-btn")
 
 let idx = 0;
 slider.appendChild(images[idx]);
@@ -26,7 +27,17 @@ for (const btn of imageButtons) {
     })
 }
 
+let isPaused = false;
+
+pauseButton.addEventListener("click", () => { 
+    isPaused = !isPaused;
+    console.log("voop")
+});
+
 const anim = setInterval(() => {
+    if (isPaused)
+        return;
+
     slider.removeChild(images[idx % images.length]);
     slider.appendChild(images[(idx + 1) % images.length]);
     idx++;

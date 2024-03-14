a9cec660db13bc3fb4d3142903e17e92d538d9c0
atmatm9182 <atmatm9182@gmail.com>
2024-03-14 11:30:48 +0100

    [LAB3] Added basic recording functionality

--- lab3/index.js
+++ lab3/index.js
@@ -6,6 +6,29 @@ for (const sound of soundElements) {
     sounds[sound.dataset.key] = sound;
 }
 
+const recordButton = document.querySelector("#record-btn");
+const recordingMap = [];
+
+recordButton.addEventListener('click', () => {
+    const recordingStartTime = Date.now();
+
+    addEventListener('keypress', e => {
+        const sound = sounds[e.key];
+        sound.currentTime = 0;
+        sound.play();
+
+        recordingMap.push({ sound, delay: Date.now() - recordingStartTime });
+    });
+});
+
+const startButton = document.querySelector("#play-recording-btn");
+startButton.addEventListener('click', () => {
+    console.log(recordingMap);
+    for (const { sound, delay } of recordingMap) {
+        setTimeout(() => { sound.currentTime = 0; sound.play() }, delay)
+    }
+});
+
 addEventListener('keypress', e => {
     const sound = sounds[e.key];
     sound.currentTime = 0;

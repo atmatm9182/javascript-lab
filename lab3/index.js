27c3fabd2a675527dde111ecb71ffb9c0bcf9bb8
atmatm9182 <atmatm9182@gmail.com>
2024-04-04 00:18:27 +0200

    [LAB3] Fixed the recording

--- lab3/index.js
+++ lab3/index.js
@@ -7,30 +7,46 @@ for (const sound of soundElements) {
 }
 
 const recordButton = document.querySelector("#record-btn");
+
 const recordingMap = [];
+
+let recordingEventListener = null;
 
 recordButton.addEventListener('click', () => {
     const recordingStartTime = Date.now();
 
-    addEventListener('keypress', e => {
+    recordingEventListener = e => {
         const sound = sounds[e.key];
         sound.currentTime = 0;
         sound.play();
 
-        recordingMap.push({ sound, delay: Date.now() - recordingStartTime });
-    });
+        recordingMap.push({ sound, delay: Date.now() - recordingStartTime })
+    };
+
+    window.addEventListener('keypress', recordingEventListener);
 });
 
 const startButton = document.querySelector("#play-recording-btn");
-startButton.addEventListener('click', () => {
-    console.log(recordingMap);
+
+function startButtonEventListener() {
     for (const { sound, delay } of recordingMap) {
         setTimeout(() => { sound.currentTime = 0; sound.play() }, delay)
     }
-});
+}
 
-addEventListener('keypress', e => {
-    const sound = sounds[e.key];
-    sound.currentTime = 0;
-    sound.play();
-})
+startButton.addEventListener('click', startButtonEventListener);
+
+const stopButton = document.querySelector("#stop-btn");
+stopButton.addEventListener('click', () => {
+    if (recordingEventListener)
+        window.removeEventListener('keypress', recordingEventListener);
+
+    window.addEventListener('keypress', e => {
+        const sound = sounds[e.key];
+        if (!sound)
+            return;
+
+        sound.currentTime = 0;
+        sound.play();
+    })
+});

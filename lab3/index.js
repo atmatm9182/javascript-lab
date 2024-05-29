3734717b39f89eb6060c780a132bb348ba0db8a6
atmatm9182 <atmatm9182@gmail.com>
2024-05-29 21:18:45 +0200

    feat(lab3): implemented the looper

--- lab3/index.js
+++ lab3/index.js
@@ -8,53 +8,63 @@ for (const sound of soundElements) {
 
 const recordButton = document.querySelector("#record-btn");
 
-const recordingMap = [];
+let recordingMap = [];
 
 let recordingEventListener = null;
 
 const looper = document.querySelector("#looper");
 const looperEnabledCheckbox = looper.getElementsByTagName("input").item(0);
 
-let looperInterval = null;
-
-looperEnabledCheckbox.addEventListener("click", () => {
-    const soundsArray = Object.values(sounds);
-    const maxSoundDuration = soundsArray.reduce((prev, cur) => {
-        return cur.duration > prev.duration ? cur : prev;
-    }, soundsArray[0]).duration;
-
-    for (const [_, sound] of Object.entries(sounds)) {
-        sound.currentTime = 0;
-        sound.play();
-    }
-
-    if (looperEnabledCheckbox.checked) {
-        looperInterval = setInterval(() => {
-            for (const [_, sound] of Object.entries(sounds)) {
-                sound.currentTime = 0;
-                sound.play();
-            }
-        }, maxSoundDuration * 1000);
-    } else {
-        clearInterval(looperInterval);
-    }
-});
+let looperIntervals = [];
 
 recordButton.addEventListener("click", () => {
+    recordingMap = [];
+
     const recordingStartTime = Date.now();
+
+    function loopedRecordingEventListener(e) {
+        const sound = sounds[e.key];
+        if (!sound) {
+            return;
+        }
+
+        sound.play();
+        recordingMap.push({
+            sound,
+            delay: Date.now() - recordingStartTime,
+        });
+
+        const interval = setInterval(() => {
+            sound.play();
+            recordingMap.push({
+                sound,
+                delay: Date.now() - recordingStartTime,
+            });
+        }, sound.duration * 1000);
+
+        looperIntervals.push(interval);
+    }
 
     recordingEventListener = (e) => {
         const sound = sounds[e.key];
+        if (!sound) {
+            return;
+        }
+
         sound.currentTime = 0;
         sound.play();
 
         recordingMap.push({ sound, delay: Date.now() - recordingStartTime });
     };
 
+    recordingEventListener = looperEnabledCheckbox.checked
+        ? loopedRecordingEventListener
+        : recordingEventListener;
+
     window.addEventListener("keypress", recordingEventListener);
 });
 
-const startButton = document.querySelector("#play-recording-btn");
+const playButton = document.querySelector("#play-recording-btn");
 
 function startButtonEventListener() {
     for (const { sound, delay } of recordingMap) {
@@ -65,20 +75,19 @@ function startButtonEventListener() {
     }
 }
 
-startButton.addEventListener("click", startButtonEventListener);
+playButton.addEventListener("click", startButtonEventListener);
 
 const stopButton = document.querySelector("#stop-btn");
 stopButton.addEventListener("click", () => {
-    if (recordingEventListener)
+    if (recordingEventListener) {
         window.removeEventListener("keypress", recordingEventListener);
+    }
 
-    window.addEventListener("keypress", (e) => {
-        const sound = sounds[e.key];
-        if (!sound) return;
+    for (const interval of looperIntervals) {
+        clearInterval(interval);
+    }
 
-        sound.currentTime = 0;
-        sound.play();
-    });
+    looperIntervals = [];
 });
 
 const metronomeSound = document.querySelector("#metronome-sound");
@@ -94,11 +103,12 @@ metronomeButton.addEventListener("click", () => {
         clearInterval(metronomeTimer);
         metronomeTimer = null;
         metronomeButton.textContent = "Play";
-    } else {
-        metronomeTimer = setInterval(() => {
-            metronomeSound.currentTime = 0;
-            metronomeSound.play();
-        }, 60000 / freq.valueAsNumber);
-        metronomeButton.textContent = "Stop";
+        return;
     }
+
+    metronomeTimer = setInterval(() => {
+        metronomeSound.currentTime = 0;
+        metronomeSound.play();
+    }, 60000 / freq.valueAsNumber);
+    metronomeButton.textContent = "Stop";
 });
